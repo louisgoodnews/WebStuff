996768fef4b5918b0f086f74cfc13de551ce968f
louisgoodnews <113772991+louisgoodnews@users.noreply.github.com>
2025-03-13 19:36:57 +0100

    Update web_service.py

--- web_service.py
+++ web_service.py
@@ -1,109 +1,311 @@
+"""
+Author: lodego
+Date: 2024-03-13
+"""
+
 import aiohttp
+import asyncio
 
 from typing import *
 
-from logger import Logger
+from utils.logger import Logger
+
+__all__: List[str] = ["web_service"]
 
 
 class WebService:
     """
-    A utility class providing static methods to perform HTTP requests.
+    A class that provides methods for sending HTTP requests.
 
     Attributes:
-        logger (Logger): An instance of the Logger class for logging messages.
+        logger: Logger
+            The logger instance for the WebService class.
     """
     logger: Logger = Logger.get_logger(name="WebService")
 
-
     @classmethod
-    async def delete(cls, url: str, log: bool = False, **kwargs,) -> Dict[str, Any] | None:
+    def delete(
+        cls,
+        url: str,
+        log: bool = False,
+        **kwargs,
+    ) -> Optional[Dict[str, Any]]:
         """
-        Performs a DELETE request to the given URL with any additional keyword arguments.
+        Sends a DELETE request to the specified URL and returns the response as a JSON.
 
-        Args:
-            url (str): The URL to which the DELETE request is sent.
-            log (bool): Whether to log the request and response. Defaults to False.
-            **kwargs: Additional keyword arguments to be passed to the DELETE request.
+        :param url: The URL to send the DELETE request to.
+        :type url: str
 
-        Returns:
-            Dict[str, Any] | None: The JSON response from the server, or None on failure.
+        :param log: A flag indicating whether to log the response status (Defaults to False).
+        :type log: bool
+
+        :param kwargs: Additional keyword arguments for the DELETE request.
+        :type kwargs: dict
+
+        :return: The JSON response from the URL, or None if an error occurs.
+        :rtype: Optional[Dict[str, Any]]
         """
         try:
-            async with aiohttp.ClientSession() as session:
-                async with session.delete(url=url, **kwargs,) as response:
-                    if log:
-                        cls.logger.info(message=f"Received response from {url}: {response.status}")
-                    return await response.json()
-        except aiohttp.ClientError as e:
-            cls.logger.error(message=f"Caught an exception while making a DELETE request to {url}: {e}")
-            return None
 
+            async def __delete__(
+                url: str,
+                log: bool = False,
+                **kwargs,
+            ) -> Optional[Dict[str, Any]]:
+                """
+                Sends a DELETE request to the specified URL and returns the response as a JSON.
+
+                :param url: The URL to send the DELETE request to.
+                :type url: str
+
+                :param log: A flag indicating whether to log the response status (Defaults to False).
+                :type log: bool
+
+                :param kwargs: Additional keyword arguments for the DELETE request.
+                :type kwargs: dict
+
+                :return: The JSON response from the URL, or None if an error occurs.
+                :rtype: Optional[Dict[str, Any]]
+                """
+                async with aiohttp.ClientSession() as session:
+                    async with session.delete(
+                        url=url,
+                        **kwargs,
+                    ) as response:
+                        if log:
+                            # Log the response status
+                            cls.logger.info(
+                                message=f"Received response from {url}: {response.status}"
+                            )
+
+                        # Return the JSON response
+                        return await response.json()
+
+            # Run the asynchronous __delete__ function
+            return asyncio.run(
+                __delete__(
+                    log=log,
+                    url=url,
+                    **kwargs,
+                )
+            )
+        except Exception as e:
+            # Log an error message indicating that an exception has occurred
+            cls.logger.error(message=f"Caught an exception while attempting to send 'DELETE' request to URL: '{url}': {e}")
+
+            # Re-raise the exception to the caller
+            raise e
 
     @classmethod
-    async def get(cls, url: str, log: bool = False, **kwargs,) -> Dict[str, Any] | None:
+    def get(
+        cls,
+        url: str,
+        log: bool = False,
+        **kwargs,
+    ) -> Optional[Dict[str, Any]]:
         """
-        Performs a GET request to the given URL with any additional keyword arguments.
+        Sends a GET request to the specified URL and returns the response as a JSON.
 
-        Args:
-            url (str): The URL to which the GET request is sent.
-            log (bool): Whether to log the request and response. Defaults to False.
-            **kwargs: Additional keyword arguments to be passed to the GET request.
+        :param url: The URL to send the GET request to.
+        :type url: str
 
-        Returns:
-            Dict[str, Any] | None: The JSON response from the server, or None on failure.
+        :param log: A flag indicating whether to log the response status (Defaults to False).
+        :type log: bool
+
+        :param kwargs: Additional keyword arguments for the GET request.
+        :type kwargs: dict
+
+        :return: The JSON response from the URL, or None if an error occurs.
+        :rtype: Optional[Dict[str, Any]]
         """
         try:
-            async with aiohttp.ClientSession() as session:
-                async with session.get(url=url, **kwargs,) as response:
-                    if log:
-                        cls.logger.info(message=f"Received response from {url}: {response.status}")
-                    return await response.json()
-        except aiohttp.ClientError as e:
-            cls.logger.error(message=f"Caught an exception while making a GET request to {url}: {e}")
-            return None
-    
+
+            async def __get__(
+                url: str,
+                log: bool = False,
+                **kwargs,
+            ) -> Optional[Dict[str, Any]]:
+                """
+                Asynchronously sends a GET request and returns the response JSON.
+
+                :param url: The URL to send the GET request to.
+                :type url: str
+
+                :param log: A flag indicating whether to log the response status.
+                :type log: bool
+
+                :param kwargs: Additional keyword arguments for the GET request.
+                :type kwargs: dict
+
+                :return: The JSON response from the URL, or None if an error occurs.
+                :rtype: Optional[Dict[str, Any]]
+                """
+                async with aiohttp.ClientSession() as session:
+                    async with session.get(
+                        url=url,
+                        **kwargs,
+                    ) as response:
+                        if log:
+                            cls.logger.info(
+                                message=f"Received response from {url}: {response.status}"
+                            )
+                        # Return the JSON response
+                        return await response.json()
+
+            # Run the asynchronous __get__ function
+            return asyncio.run(
+                __get__(
+                    log=log,
+                    url=url,
+                    **kwargs,
+                )
+            )
+        except Exception as e:
+            # Log an error message indicating that an exception has occurred
+            cls.logger.error(message=f"Caught an exception while attempting to send 'GET' request to URL: '{url}': {e}")
+
+            # Re-raise the exception to the caller
+            raise e
+
     @classmethod
-    async def post(cls, url: str, log: bool = False, **kwargs,) -> Dict[str, Any] | None:
+    def post(
+        cls,
+        url: str,
+        log: bool = False,
+        **kwargs,
+    ) -> Optional[Dict[str, Any]]:
         """
-        Performs a POST request to the given URL with any additional keyword arguments.
+        Sends a POST request to the specified URL and returns the response as a JSON.
 
-        Args:
-            url (str): The URL to which the POST request is sent.
-            log (bool): Whether to log the request and response. Defaults to False.
-            **kwargs: Additional keyword arguments to be passed to the POST request.
+        :param url: The URL to send the POST request to.
+        :type url: str
 
-        Returns:
-            Dict[str, Any] | None: The JSON response from the server, or None on failure.
+        :param log: A flag indicating whether to log the response status (Defaults to False).
+        :type log: bool
+
+        :param kwargs: Additional keyword arguments for the POST request.
+        :type kwargs: dict
+
+        :return: The JSON response from the URL, or None if an error occurs.
+        :rtype: Optional[Dict[str, Any]]
         """
         try:
-            async with aiohttp.ClientSession() as session:
-                async with session.post(url=url, **kwargs,) as response:
-                    if log:
-                        cls.logger.info(message=f"Received response from {url}: {response.status}")
-                    return await response.json()
-        except aiohttp.ClientError as e:
-            cls.logger.error(message=f"Caught an exception while making a POST request to {url}: {e}")
-            return None
-    
+
+            async def __post__(
+                url: str,
+                log: bool = False,
+                **kwargs,
+            ) -> Optional[Dict[str, Any]]:
+                """
+                Asynchronously sends a POST request to the specified URL and returns the response as a JSON.
+
+                :param url: The URL to send the POST request to.
+                :type url: str
+
+                :param log: A flag indicating whether to log the response status (Defaults to False).
+                :type log: bool
+
+                :param kwargs: Additional keyword arguments for the POST request.
+                :type kwargs: dict
+
+                :return: The JSON response from the URL, or None if an error occurs.
+                :rtype: Optional[Dict[str, Any]]
+                """
+                async with aiohttp.ClientSession() as session:
+                    async with session.post(
+                        url=url,
+                        **kwargs,
+                    ) as response:
+                        if log:
+                            cls.logger.info(
+                                message=f"Received response from {url}: {response.status}"
+                            )
+                        # Return the JSON response
+                        return await response.json()
+
+            # Run the asynchronous __post__ function
+            return asyncio.run(
+                __post__(
+                    log=log,
+                    url=url,
+                    **kwargs,
+                )
+            )
+        except Exception as e:
+            # Log an error message indicating that an exception has occurred
+            cls.logger.error(message=f"Caught an exception while attempting to send 'POST' request to URL: '{url}': {e}")
+
+            # Re-raise the exception to the caller
+            raise e
+
     @classmethod
-    async def put(cls, url: str, log: bool = False, **kwargs,) -> Dict[str, Any] | None:
+    def put(
+        cls,
+        url: str,
+        log: bool = False,
+        **kwargs,
+    ) -> Optional[Dict[str, Any]]:
         """
-        Performs a PUT request to the given URL with any additional keyword arguments.
+        Sends a PUT request to the specified URL and returns the response as a JSON.
 
-        Args:
-            url (str): The URL to which the PUT request is sent.
-            log (bool): Whether to log the request and response. Defaults to False.
-            **kwargs: Additional keyword arguments to be passed to the PUT request.
+        :param url: The URL to send the PUT request to.
+        :type url: str
 
-        Returns:
-            Dict[str, Any] | None: The JSON response from the server, or None on failure.
+        :param log: A flag indicating whether to log the response status (Defaults to False).
+        :type log: bool
+
+        :param kwargs: Additional keyword arguments for the PUT request.
+        :type kwargs: dict
+
+        :return: The JSON response from the URL, or None if an error occurs.
+        :rtype: Optional[Dict[str, Any]]
         """
         try:
-            async with aiohttp.ClientSession() as session:
-                async with session.put(url=url, **kwargs,) as response:
-                    if log:
-                        cls.logger.info(message=f"Received response from {url}: {response.status}")
-                    return await response.json()
-        except aiohttp.ClientError as e:
-            cls.logger.error(message=f"Caught an exception while making a PUT request to {url}: {e}")
-            return None
+
+            async def __put__(
+                url: str,
+                log: bool = False,
+                **kwargs,
+            ) -> Optional[Dict[str, Any]]:
+                """
+                Asynchronously sends a PUT request to the specified URL and returns the response as a JSON.
+
+                :param url: The URL to send the PUT request to.
+                :type url: str
+
+                :param log: A flag indicating whether to log the response status (Defaults to False).
+                :type log: bool
+
+                :param kwargs: Additional keyword arguments for the PUT request.
+                :type kwargs: dict
+
+                :return: The JSON response from the URL, or None if an error occurs.
+                :rtype: Optional[Dict[str, Any]]
+                """
+                async with aiohttp.ClientSession() as session:
+                    async with session.put(
+                        url=url,
+                        **kwargs,
+                    ) as response:
+                        if log:
+                            cls.logger.info(
+                                message=f"Received response from {url}: {response.status}"
+                            )
+
+                        # Return the JSON response
+                        return await response.json()
+
+            # Run the asynchronous __put__ function
+            return asyncio.run(
+                __put__(
+                    log=log,
+                    url=url,
+                    **kwargs,
+                )
+            )
+        except Exception as e:
+            # Log an error message indicating that an exception has occurred
+            cls.logger.error(message=f"Caught an exception while attempting to send 'PUT' request to URL: '{url}': {e}")
+
+            # Re-raise the exception to the caller
+            raise e
